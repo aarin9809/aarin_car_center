e45127f8fed2643361a8db0b4cdbe5c79e4c977d
aarin9809 <sylee9809@naver.com>
2023-05-05 21:58:44 +0900

    implenemt: 차량 입고함수 기능 구현
    - 파일 입출력 구현
    - 예외처리 구현

--- src/AarinCarCenter.java
+++ src/AarinCarCenter.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 public class AarinCarCenter {
     void InputCar(String carType,
                   Integer carYear,
@@ -5,8 +7,7 @@ public class AarinCarCenter {
                   String color,
                   Integer mileage,
                   String accident,
-                  Integer price)
-    {
+                  Integer price) throws IOException,FileNotFoundException {
         System.out.println("입력하신 차종:" + carType);
         System.out.println("입력하신 연식:" + carYear);
         System.out.println("입력하신 연료타입:" + fuelType);
@@ -14,5 +15,19 @@ public class AarinCarCenter {
         System.out.println("입력하신 주행거리:" + mileage);
         System.out.println("입력하신 사고유무:" + accident);
         System.out.println("입력하신 가격:" + price);
+
+        // 차량 정보를 파일로 저장
+
+        PrintWriter pw = new PrintWriter(new FileWriter("./db/car_data.txt", true));
+
+        String carInfo = carType + ",";
+        carInfo+= carYear.toString() + ",";
+        carInfo+= fuelType + ",";
+        carInfo+= color + ",";
+        carInfo+= mileage.toString() + ",";
+        carInfo+= accident + ",";
+        carInfo+= price.toString();
+        pw.println(carInfo);
+        pw.close();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 // ⇧을(를) 두 번 눌러 전체 검색 대화상자를 열고 'show whitespaces'를 입력한 다음,
 // Enter를 누르세요. 그러면 코드 내에서 공백 문자를 확인할 수 있습니다.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String carType = "";
         Integer carYear = 0;
         String fuelType = "";
@@ -14,33 +17,40 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("차 종류: ");
-        carType = sc.nextLine();
-
-        System.out.print("연식: ");
-        carYear = Integer.parseInt(sc.nextLine());
-
-        System.out.print("연료타입: ");
-        fuelType = sc.nextLine();
-
-        System.out.print("색상: ");
-        color = sc.nextLine();
-
-        System.out.print("주행거리: ");
-        mileage = Integer.parseInt(sc.nextLine());
-
-        System.out.print("사고유무: ");
-        accident = sc.nextLine();
-
-        System.out.print("판매가격: ");
-        price = Integer.parseInt(sc.nextLine());
 
 
         AarinCarCenter carCenter1 = new AarinCarCenter();
 
-        carCenter1.InputCar(carType, carYear, fuelType, color, mileage, accident,price);
+        try {
+            for (int i=0; i<2; i++){
+                System.out.print("차 종류: ");
+                carType = sc.nextLine();
 
+                System.out.print("연식: ");
+                carYear = Integer.parseInt(sc.nextLine());
 
+                System.out.print("연료타입: ");
+                fuelType = sc.nextLine();
+
+                System.out.print("색상: ");
+                color = sc.nextLine();
+
+                System.out.print("주행거리: ");
+                mileage = Integer.parseInt(sc.nextLine());
+
+                System.out.print("사고유무: ");
+                accident = sc.nextLine();
+
+                System.out.print("판매가격: ");
+                price = Integer.parseInt(sc.nextLine());
+
+                carCenter1.InputCar(carType, carYear, fuelType, color, mileage, accident, price);
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("FileOUtputStream Error:"+ e.getMessage() + e.getStackTrace());
+        } catch (IOException e){
+            System.out.println("IOException Error:"+ e.getMessage() + e.getStackTrace());
+        }
 
     }
 }
